d44dafdb7db499daf7edebfbe4852b9545f36090
Arnaud Candaele <arnaud.candaele@bbri.be>
2021-05-26 16:48:00 +0200

    Finalize comments menu

--- ReflectViewer/Assets/Scripts/BIMEXPO/Web.cs
+++ ReflectViewer/Assets/Scripts/BIMEXPO/Web.cs
@@ -654,4 +654,99 @@ public class Web : MonoBehaviour
             throw new Exception("Couldn't extract the tile price from the DB!");
         }
     }
+
+    public void saveComment(string comment, GameObject surface)
+    {
+        // Getting surface ID
+        var meta = surface.GetComponent<Metadata>();
+        string surfaceID = null;
+        if (meta != null)
+        {
+            surfaceID = meta.GetParameter("Id");
+        }
+        else
+        {
+            throw new Exception("No Id parameter found on surface!");
+        }
+
+        string phpScript = "http://bimexpo/CreateComment.php";
+        WWWForm form = new WWWForm();
+        form.AddField("projectId", projectId);
+        form.AddField("clientId", clientId);
+        form.AddField("comment", comment);
+        form.AddField("surfaceID", surfaceID);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(phpScript, form))
+        {
+            www.SendWebRequest();
+            while (www.result == UnityWebRequest.Result.InProgress)
+            {
+                // Just wait
+            }
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string receivedTilesString = www.downloadHandler.text;
+            }
+        }
+    }
+
+    public string GetComment(GameObject surface)
+    {
+        // Getting surface ID
+        var meta = surface.GetComponent<Metadata>();
+        string surfaceID = null;
+        if (meta != null)
+        {
+            surfaceID = meta.GetParameter("Id");
+        }
+        else
+        {
+            throw new Exception("No Id parameter found on surface!");
+        }
+
+        string phpScript = "http://bimexpo/GetComment.php";
+        string[] phpReturnedList = { };
+        WWWForm form = new WWWForm();
+        form.AddField("projectId", projectId);
+        form.AddField("clientId", clientId);
+        form.AddField("surfaceID", surfaceID);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(phpScript, form))
+        {
+            www.SendWebRequest();
+            while (www.result == UnityWebRequest.Result.InProgress)
+            {
+                // Just wait
+            }
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string receivedTilesString = www.downloadHandler.text;
+                phpReturnedList = receivedTilesString.Split(';');
+            }
+        }
+
+        bool startRecordingResults = false;
+        string comment = "";
+
+        foreach (string item in phpReturnedList)
+        {
+            if (startRecordingResults)
+            {
+                comment = item;
+            }
+            if (item.Contains("RETURNS"))
+            {
+                startRecordingResults = true;
+            }
+        }
+        return comment;
+    }
 }
